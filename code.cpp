285b66d0fbc95def40879c17cb3bf58526977118
Ayush Gupta <ayush.guptaa.14@gmail.com>
2020-04-07 01:30:10 +0530

    Updated with new C++ snippet

--- code.cpp
+++ code.cpp
@@ -1,38 +1,28 @@
 #include <bits/stdc++.h>
 
-#define For(a,b,c,d)  for(int a = b; a < c; a += d)
-#define Forr(a,b,c,d) for(int a = b; a >= c; a -= d)
-#define Fill(A,x)     memset(A,x,sizeof(A))
-#define ll            long long
-#define ull           unsigned long long
-#define ld            long double
-#define pb            push_back
-#define mp            make_pair
+#define For(a, b, c, d)    for(int a = b; a < c; a += d)
+#define Forr(a, b, c, d)   for(int a = b; a >= c; a -= d)
+#define ll                 long long
+#define trace(args...)     std::string arg_str = #args; std::replace(arg_str.begin(), arg_str.end(), ',', ' '); std::stringstream args_str(arg_str); std::istream_iterator<std::string> it(args_str); track(it, args);
+
+void track(std::istream_iterator<std::string> it) { std::clog << "\b \b\b \b\n"; }
+
+template <typename type, typename... types>
+
+void track(std::istream_iterator<std::string> it, type arg, const types&... args) {
+	std::clog << *it << " = " << arg << ", ";
+	track(++it, args...);
+}
 
 using namespace std;
 
-const ll mod = 1e9 + 7;
-
-int main(){
+int main() {
 	#ifdef ONLINE_JUDGE
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	#endif
-	
-	#ifdef LOCAL_TEST
-	freopen("testfile.txt","r",stdin);
-	freopen("Aoutput.txt","w",stdout);
-	freopen("debug.txt","w",stderr);
-	clock_t start = clock();
+		std::ios_base::sync_with_stdio(false);
+		std::cin.tie(NULL);
 	#endif
 
-	
 
-	#ifdef LOCAL_TEST
-	clock_t stop = clock();
-	double runtime = (double)(stop-start)/CLOCKS_PER_SEC;
-	freopen("time.txt","w",stdout);
-	printf("\n%sExecution time : %lf\n%s","\x1B[34m",runtime,"\x1B[0m");
-	#endif
+
 	return 0;
 }
